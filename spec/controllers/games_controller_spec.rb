aa8c9bdc304fe3e93510aee7b5d47433b161dc06
Gustavo Barron <cicloid@42fu.com>
2018-04-18 09:18:40 -0500

    Commiting low hanging tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   include ApiDoc::Games::Api
 
-  describe 'GET #index' do
+  describe 'GET #show' do
     include ApiDoc::Games::Index
 
     it 'returns a list of games', :dox do
-      get :index
+      last = Game.create
+      get :show, params: { id: last.id }
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'POST #create' do
+    include ApiDoc::Games::Create
+
+    it 'returns a list of games', :dox do
+      post :create
       expect(response).to have_http_status(:ok)
     end
   end
--- spec/features/play_a_game_spec.rb
+++ spec/features/play_a_game_spec.rb
@@ -2,6 +2,6 @@ RSpec.describe "Play a Game", js: true, type: :feature do
 
   it "shows a game" do
     visit '/'
-    expect(page).to have_content 'Play a Game'
+    expect(page).to have_content 'New Game'
   end
 end
