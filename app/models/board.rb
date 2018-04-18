762f7645c64e432f5de1bacba942d99f395b043f
Gustavo Barron <cicloid@42fu.com>
2018-04-18 02:00:34 -0500

    Added Minesweeper dingus Didn't know how to play Minesweeper

---
+++ app/models/board.rb
@@ -0,0 +1,130 @@
+class Board
+  attr_accessor :rows, :cols, :grid, :mines
+
+  SURROUND = [-1, 0, 1].product([-1, 0, 1]) - [[0, 0]]
+
+  DIFFICULTY = {
+    beginner: [8, 8, 10],
+    intermediate: [16, 16, 40],
+    advanced: [24, 24, 99]
+  }
+
+  def initialize(level: :beginner)
+    @rows, @cols, @mines = DIFFICULTY[level.to_sym]
+    @level = level
+
+    @grid = Array.new(@rows) {|row|
+      Array.new(@cols) {|col|
+        Cell.new(row, col)
+      }
+    }
+  end
+
+  def lost?
+    @lost
+  end
+
+  def won?
+    @grid.each do |row|
+      row.each do |cell|
+        return false if cell.revealed? && !cell.boobytrapped?
+      end
+    end
+    true
+  end
+
+  def boobytrap_the_board!
+    current_mines = 0
+    while current_mines < @mines
+      rand_row = rand(@rows)
+      rand_col = rand(@cols)
+
+      cell = @grid[rand_row][rand_col]
+      unless cell.boobytrapped?
+        cell.mine = true
+        current_mines += 1
+      end
+    end
+  end
+
+  def reveal(row, col)
+    if @grid[row][col].boobytrapped?
+      @lost = true
+    else
+      reveal_tiles(row, col)
+    end
+  end
+
+  def get_adjecent_mines(row, col)
+    neighbors = find_neighbors(row, col)
+    neighbors.each do |neighbor|
+      row2, col2 = neighbor
+      @grid[row][col].adjacent_mines += 1 if @grid[row2][col2].boobytrapped?
+    end
+    @grid[row][col].adjacent_mines
+  end
+
+  def reveal_tiles(row, col)
+    return if @grid[row][col].flagged?
+    @grid[row][col].reveal!
+    cell_mines = get_adjecent_mines(row, col)
+    return if cell_mines > 0
+    neighbors = find_neighbors(row, col)
+    neighbors.each do |row, col|
+      reveal_tiles(row, col) unless @grid[row][col].revealed? || @grid[row][col].boobytrapped?
+    end
+  end
+
+  def find_neighbors(row, col)
+    neighbors = []
+    SURROUND.each do |pos|
+      drow, dcol = [pos[0]+row, pos[1]+col]
+      neighbors << [drow, dcol] unless out_of_bounds?(drow, dcol)
+    end
+    neighbors
+  end
+
+  def collect_cells_hashes
+    @grid.collect do |cols|
+      row = cols.collect do |cell|
+        cell.to_hash
+      end
+    end
+  end
+
+  def to_json
+    {
+      'difficulty': @level,
+      'rows': @rows,
+      'cols': @cols,
+      'cells': collect_cells_hashes
+    }.to_json
+  end
+
+  def to_s
+    string = []
+    @grid.each do |cols|
+      row = cols.collect do |cell|
+        if cell.boobytrapped? && cell.revealed?
+          "M"
+        elsif cell.flagged?
+          "F"
+        elsif cell.adjacent_mines > 0 && cell.revealed?
+          cell.adjacent_mines
+        elsif cell.revealed?
+          " "
+        else
+          "-"
+        end
+      end
+      string << "#{row.join}"
+    end
+    string.join("\r\n")
+  end
+
+  def out_of_bounds?(row, col)
+    # TODO: This should be more specific, since the board is a square, there
+    # is no problem for now
+    [row, col].any? { |el| el < 0  || (el > @rows - 1)}
+  end
+end
---
+++ app/models/cell.rb
@@ -0,0 +1,48 @@
+class Cell
+  attr_accessor :x, :y, :mine, :revealed, :flagged, :adjacent_mines
+
+  class << self
+    def load_from_hash(hash)
+      n = new hash['x'], hash['y'], hash['mine']
+      n.revealed = hash['revealed']
+      n.flagged = hash['flagged']
+      n.adjacent_mines = hash['adjacent_mines']
+    end
+  end
+
+  def initialize(x, y, mine = false)
+    @x = x
+    @y = y
+    @mine = mine
+    @revealed = false
+    @flagged = false
+    @adjacent_mines = 0
+  end
+
+  def boobytrapped?
+    @mine == true
+  end
+
+  def flagged?
+    @flagged == true
+  end
+
+  def revealed?
+    @revealed == true
+  end
+
+  def reveal!
+    @revealed = true
+  end
+
+  def to_hash
+    {
+      'x': @x,
+      'y': @y,
+      'mine': @mine,
+      'revealed': @revealed,
+      'flagged': @flagged
+    }
+  end
+
+end
